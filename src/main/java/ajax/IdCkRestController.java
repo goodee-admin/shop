d3fd9bca2b6453717d796de98e57dfe976d5ef29
GD12 <GD12@gd>
2025-11-04 15:59:00 +0900

    회원가입, 사원추가 AJax 구현 추가 구현중

---
+++ src/main/java/ajax/IdCkRestController.java
@@ -0,0 +1,20 @@
+package ajax;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/IdCk")
+public class IdCkRestController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		
+		// CustomerDao.selectCustomerIdCheck()
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/view/out/addMember.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>shop</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+
+<body>
+	<div>
+		<input type="text" id="idck">
+		<button type="button">아이디 사용 여부</button><!-- ajax 호출 -->
+	</div>
+</body>
+</html>
