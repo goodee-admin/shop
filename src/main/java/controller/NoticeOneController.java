73e6a666c9027335914d862723f3872bafc2a470
GD12 <GD12@gd>
2025-11-07 11:03:24 +0900

    공지리스트 페이지페이징 공지CRUD구현중 인터페이스설계완료

---
+++ src/main/java/controller/NoticeOneController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/emp/noticeOne")
+public class NoticeOneController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
